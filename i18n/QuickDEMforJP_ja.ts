491b2d0d813ac45f5decf2b3d2fe944f5e704d36
bordoray <bordoray@gmail.com>
2024-08-07 16:38:37 +0900

    i18n

---
+++ i18n/QuickDEMforJP_ja.ts
@@ -0,0 +1,240 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../progress_dialog.ui" line="20"/>
+        <source>Processing...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.ui" line="44"/>
+        <source>%v/%m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.ui" line="51"/>
+        <source>Progressing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.ui" line="69"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProgressDialog</name>
+    <message>
+        <location filename="../progress_dialog.py" line="27"/>
+        <source>Initializing process...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="31"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="41"/>
+        <source>Cancelling...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="63"/>
+        <source>Set the output file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="66"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="68"/>
+        <source>Finalizing...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="70"/>
+        <source>Aborting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="72"/>
+        <source>Are you sure to cancel process?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="74"/>
+        <source>Output format is not checked.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="77"/>
+        <source>Input DEM path is not defined.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="80"/>
+        <source>GeoTIFF output path is not defined.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="83"/>
+        <source>Terrain RGB output path is not defined.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="86"/>
+        <source>CRS of output file is not defined.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="89"/>
+        <source>Cannot find output folder.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="91"/>
+        <source>Completed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="94"/>
+        <source>Process completed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="99"/>
+        <source>No XML file found in input folder.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="103"/>
+        <source>No XML file found in input zip file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="110"/>
+        <source>Only ZIP file, XML file, or folder conatining XML files are allowed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="115"/>
+        <source>Only XML file format is allowed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="118"/>
+        <source>Incorrect XML file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="121"/>
+        <source>Mixed mesh format (2nd mesh and 3rd mesh)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="125"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="127"/>
+        <source>Incorrect Mmsh code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="132"/>
+        <source>Image size is too large</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuickDEM4JP</name>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="14"/>
+        <source>QuickDEM4JP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="20"/>
+        <source>Input settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="40"/>
+        <source>Format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="47"/>
+        <source>&apos;xml&apos; or zip file including &apos;xml&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="60"/>
+        <source>Folder including &apos;xml&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="86"/>
+        <source>DEM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="108"/>
+        <source>Output settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="122"/>
+        <source>GeoTiff</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="199"/>
+        <source>Output path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="174"/>
+        <source>Terrain RGB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="235"/>
+        <source>CRS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="263"/>
+        <source>Set 0m to sea area</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="282"/>
+        <source>Add output layers after process</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp_dialog_base.ui" line="300"/>
+        <source>Download page</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuickDEMforJP</name>
+    <message>
+        <location filename="../quick_dem_for_jp.py" line="123"/>
+        <source>&amp;Quick_DEM_for_JP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quick_dem_for_jp.py" line="111"/>
+        <source>Quick_DEM_for_JP</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
