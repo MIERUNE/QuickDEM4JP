f67355c0e0f0441111cd67be41f52c4ab2bc9562
bordoray <bordoray@gmail.com>
2024-08-08 09:56:54 +0900

    redo i18n

--- i18n/QuickDEMforJP_ja.ts
+++ i18n/QuickDEMforJP_ja.ts
@@ -52,101 +52,111 @@
     </message>
     <message>
         <location filename="../progress_dialog.py" line="68"/>
-        <source>Finalizing...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="70"/>
         <source>Aborting</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="72"/>
+        <location filename="../progress_dialog.py" line="70"/>
         <source>Are you sure to cancel process?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="74"/>
+        <location filename="../progress_dialog.py" line="72"/>
         <source>Output format is not checked.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="77"/>
+        <location filename="../progress_dialog.py" line="75"/>
         <source>Input DEM path is not defined.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="80"/>
+        <location filename="../progress_dialog.py" line="78"/>
         <source>GeoTIFF output path is not defined.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="83"/>
+        <location filename="../progress_dialog.py" line="81"/>
         <source>Terrain RGB output path is not defined.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="86"/>
+        <location filename="../progress_dialog.py" line="84"/>
         <source>CRS of output file is not defined.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="89"/>
+        <location filename="../progress_dialog.py" line="87"/>
         <source>Cannot find output folder.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="91"/>
+        <location filename="../progress_dialog.py" line="89"/>
         <source>Completed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="94"/>
+        <location filename="../progress_dialog.py" line="92"/>
         <source>Process completed.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../progress_dialog.py" line="97"/>
+        <source>Converting XML files to Terrain RGB...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../progress_dialog.py" line="99"/>
+        <source>Converting XML files to GeoTIFF DEM...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="101"/>
+        <source>Creating TIFF file...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../progress_dialog.py" line="104"/>
         <source>No XML file found in input folder.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="103"/>
+        <location filename="../progress_dialog.py" line="108"/>
         <source>No XML file found in input zip file.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="110"/>
+        <location filename="../progress_dialog.py" line="115"/>
         <source>Only ZIP file, XML file, or folder conatining XML files are allowed.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="115"/>
+        <location filename="../progress_dialog.py" line="120"/>
         <source>Only XML file format is allowed.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="118"/>
+        <location filename="../progress_dialog.py" line="123"/>
         <source>Incorrect XML file.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="121"/>
+        <location filename="../progress_dialog.py" line="126"/>
         <source>Mixed mesh format (2nd mesh and 3rd mesh)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="125"/>
+        <location filename="../progress_dialog.py" line="130"/>
         <source>Warning</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="127"/>
-        <source>Incorrect Mmsh code</source>
+        <location filename="../progress_dialog.py" line="132"/>
+        <source>Incorrect Mesh code</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../progress_dialog.py" line="132"/>
+        <location filename="../progress_dialog.py" line="137"/>
         <source>Image size is too large</source>
         <translation type="unfinished"></translation>
     </message>
