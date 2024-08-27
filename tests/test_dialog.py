e6ecbe4102258cda075df851f2abdda5da738003
bordoray <bordoray@gmail.com>
2024-08-27 10:40:20 +0900

    Add testfiles

---
+++ tests/test_dialog.py
@@ -0,0 +1,10 @@
+from qgis.core import QgsApplication
+from qgis.gui import QgisInterface
+
+from quick_dem_for_jp import QuickDEMforJP
+from contents import Contents
+
+
+def test_show_dialog(plugin: QuickDEMforJP):
+    plugin.dialog_show()
+    assert isinstance(plugin.contents, Contents)
