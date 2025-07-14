a869f7c360579973701587aa6080e46cdc3b279b
bordoray <bordoray@gmail.com>
2025-07-14 20:19:35 +0900

    Restore i18n

--- i18n/QuickDEMforJP_ja.ts
+++ i18n/QuickDEMforJP_ja.ts
@@ -1,255 +1,70 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ja" sourcelanguage="en">
+<TS version="2.1" language="ja_JP">
 <context>
-    <name>Dialog</name>
+    <name>QuickDEMforJPProcessingAlgorithm</name>
     <message>
-        <location filename="../progress_dialog.ui" line="20"/>
-        <source>Processing...</source>
-        <translation type="unfinished">処理中...</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.ui" line="44"/>
-        <source>%v/%m</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.ui" line="51"/>
-        <source>Progressing</source>
-        <translation type="unfinished">処理中</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.ui" line="69"/>
-        <source>Cancel</source>
-        <translation type="unfinished">中断</translation>
-    </message>
-</context>
-<context>
-    <name>ProgressDialog</name>
-    <message>
-        <location filename="../progress_dialog.py" line="27"/>
-        <source>Initializing process...</source>
-        <translation type="unfinished">処理開始中...</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="31"/>
-        <source>Cancel</source>
-        <translation type="unfinished">中断</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="41"/>
-        <source>Cancelling...</source>
-        <translation type="unfinished">中断待機中...</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="63"/>
-        <source>Set the output file</source>
-        <translation type="unfinished">保存ファイルを選択してください</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="64"/>
-        <source>Error</source>
-        <translation type="unfinished">エラー</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="65"/>
-        <source>Aborting</source>
-        <translation type="unfinished">確認</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="66"/>
-        <source>Are you sure to cancel process?</source>
-        <translation type="unfinished">処理を中断し、以降の処理をスキップしてよろしいですか？</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="69"/>
-        <source>Output format is not checked.</source>
-        <translation type="unfinished">出力形式にチェックを入れてください</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="70"/>
-        <source>Input DEM path is not defined.</source>
-        <translation type="unfinished">DEMの入力先パスを入力してください</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="71"/>
-        <source>GeoTIFF output path is not defined.</source>
-        <translation type="unfinished">GeoTIFFの出力先パスを入力してください</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="74"/>
-        <source>Terrain RGB output path is not defined.</source>
-        <translation type="unfinished">Terrain RGBの出力先パスを入力してください</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="77"/>
-        <source>CRS of output file is not defined.</source>
-        <translation type="unfinished">DEMの出力CRSを入力してください</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="80"/>
-        <source>Cannot find output folder.</source>
-        <translation type="unfinished">出力フォルダー先が存在していません。</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="81"/>
-        <source>Completed</source>
-        <translation type="unfinished">完了</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="82"/>
-        <source>Process completed.</source>
-        <translation type="unfinished">処理が完了しました</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="84"/>
-        <source>Converting XML files to Terrain RGB...</source>
-        <translation type="unfinished">XMLファイルからTerrain RGBへの変換処理中...</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="87"/>
-        <source>Converting XML files to GeoTIFF DEM...</source>
-        <translation type="unfinished">XMLファイルからGeoTIFFへの変換処理中...</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="90"/>
-        <source>Creating TIFF file...</source>
-        <translation type="unfinished">TIFFファイルを作成中...</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="91"/>
-        <source>No XML file found in input folder.</source>
-        <translation type="unfinished">指定ディレクトリに.xmlが存在しません</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="94"/>
-        <source>No XML file found in input zip file.</source>
-        <translation type="unfinished">指定のパスにxmlファイルが存在しません</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="97"/>
-        <source>Only ZIP file, XML file, or folder conatining XML files are allowed.</source>
-        <translation type="unfinished">指定できる形式は「xml」「.xmlが格納されたディレクトリ」「.xmlが格納された.zip」のみです</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="100"/>
-        <source>Only XML file format is allowed.</source>
-        <translation type="unfinished">指定できる形式は.xmlのみです</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="103"/>
-        <source>Incorrect XML file.</source>
-        <translation type="unfinished">不正なxmlです</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="104"/>
-        <source>Mixed mesh format (2nd mesh and 3rd mesh)</source>
-        <translation type="unfinished">2次メッシュと3次メッシュが混合しています。</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="110"/>
-        <source>Warning</source>
-        <translation type="unfinished">確認</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="118"/>
-        <source>Incorrect Mesh code</source>
-        <translation type="unfinished">メッシュコードが不正です</translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="123"/>
-        <source>Image size is too large</source>
-        <translation type="unfinished">セルサイズが大きすぎます </translation>
-    </message>
-    <message>
-        <location filename="../progress_dialog.py" line="107"/>
-        <source>Output DEM has no elevation data.</source>
-        <translation type="unfinished">出力DEMデータに標高データが含まれていません。</translation>
-    </message>
-</context>
-<context>
-    <name>QuickDEM4JP</name>
-    <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="14"/>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="62"/>
         <source>QuickDEM4JP</source>
         <translation type="unfinished">QuickDEM4JP</translation>
     </message>
     <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="20"/>
-        <source>Input settings</source>
-        <translation type="unfinished">入力設定</translation>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="65"/>
+        <source>Geospatial Information Authority of Japan(GSI) provides 1m, 5m and 10m DEM XML files of Japan &lt;a href=&apos;https://service.gsi.go.jp/kiban/&apos;&gt;on the web&lt;/a&gt;. This plugin imports XML files or XML included zip files and converts them to DEM GeoTiff and/or Terrain RGB format GeoTiff.
+</source>
+        <translation type="unfinished">国土地理院が提供する&lt;a href=&apos;https://service.gsi.go.jp/kiban/&apos;&gt;基盤地図情報数値標高モデル(DEM)&lt;/a&gt;のXML形式及びそのZIPファイルを GeoTIFF形式のDEMとTerrain RGBに変換します。</translation>
     </message>
     <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="40"/>
-        <source>Format</source>
-        <translation type="unfinished">形式</translation>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="74"/>
+        <source>DEM file (.xml or .zip containing .xml)</source>
+        <translation type="unfinished">DEMファイル（.xmlを含むzip/.xml）</translation>
     </message>
     <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="47"/>
-        <source>&apos;xml&apos; or zip file including &apos;xml&apos;</source>
-        <translation type="unfinished">&apos;xml&apos;または&apos;xml&apos;を含む&apos;zip&apos;</translation>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="74"/>
+        <source>DEM (*.xml *.zip)</source>
+        <translation type="unfinished">DEM (*.xml *.zip)</translation>
     </message>
     <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="60"/>
-        <source>Folder including &apos;xml&apos;</source>
-        <translation type="unfinished">&apos;xml&apos;を含むフォルダ</translation>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="82"/>
+        <source>Output DEM Geotiff</source>
+        <translation type="unfinished">GeoTiffの出力先</translation>
     </message>
     <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="86"/>
-        <source>DEM</source>
-        <translation type="unfinished">DEM</translation>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="88"/>
+        <source>Output Terrain RGB</source>
+        <translation type="unfinished">Terrain RGBの出力先</translation>
     </message>
     <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="108"/>
-        <source>Output settings</source>
-        <translation type="unfinished">出力設定</translation>
-    </message>
-    <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="122"/>
-        <source>GeoTiff</source>
-        <translation type="unfinished">GeoTiff</translation>
-    </message>
-    <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="199"/>
-        <source>Output path</source>
-        <translation type="unfinished">出力先</translation>
-    </message>
-    <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="174"/>
-        <source>Terrain RGB</source>
-        <translation type="unfinished">Terrain RGB</translation>
-    </message>
-    <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="235"/>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="96"/>
         <source>CRS</source>
-        <translation type="unfinished">CRS</translation>
+        <translation type="unfinished">座標参照系（CRS）</translation>
     </message>
     <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="263"/>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="101"/>
         <source>Set 0m to sea area</source>
         <translation type="unfinished">海域標高を0mに設定する</translation>
     </message>
     <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="282"/>
-        <source>Add output layers after process</source>
-        <translation type="unfinished">アルゴリズムの終了後、QGIS上で出力ファイルを開く</translation>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="122"/>
+        <source>DEM Geotiff and/or Terrain RGB output are not defined.</source>
+        <translation type="unfinished">出力ファイルが設定されていません。GeoTiffかTerrain RGBの出力を指定してください</translation>
     </message>
     <message>
-        <location filename="../quick_dem_for_jp_dialog_base.ui" line="300"/>
-        <source>Download page</source>
-        <translation type="unfinished">ダウンロードページ</translation>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="162"/>
+        <source>Conversion completed.</source>
+        <translation type="unfinished">変換が正常に完了しました。</translation>
+    </message>
+    <message>
+        <location filename="../processing_provider/quick_dem_for_jp_algorithm.py" line="165"/>
+        <source>An error occured: {}</source>
+        <translation type="unfinished">変換中にエラーが発生しました: {}</translation>
     </message>
 </context>
 <context>
-    <name>QuickDEMforJP</name>
+    <name>QuickDEMforJPProvider</name>
     <message>
-        <location filename="../quick_dem_for_jp.py" line="123"/>
-        <source>&amp;Quick_DEM_for_JP</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../quick_dem_for_jp.py" line="111"/>
-        <source>Quick_DEM_for_JP</source>
+        <location filename="../processing_provider/quick_dem_for_jp_provider.py" line="18"/>
+        <source>QuickDEM4JP</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
---
+++ quick_dem_for_jp.pro
@@ -0,0 +1,5 @@
+SOURCES += ./processing_provider/quick_dem_for_jp_algorithm.py \
+            ./processing_provider/quick_dem_for_jp_provider.py \
+            ./plugin.py
+
+TRANSLATIONS = ./i18n/QuickDEMforJP_ja.ts
