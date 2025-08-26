82c17ce981dd5f77b7a135e6d9bcc329a172a2d5
bordoray <bordoray@gmail.com>
2025-08-26 14:41:45 +0900

    Fix test

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -2,14 +2,14 @@ from qgis.core import QgsApplication
 from qgis.PyQt.QtGui import QIcon
 
 
-def test_registered(qgis_app: QgsApplication, provider: str):
-    registory = QgsApplication.processingRegistry()
-    provider = registory.providerById("quickdemforjp")
+def test_registered(provider: str):
+    registry = QgsApplication.processingRegistry()
+    provider = registry.providerById("quickdemforjp")
     assert provider is not None
     assert len(provider.name()) > 0
     assert isinstance(provider.icon(), QIcon)
 
-    alg = registory.algorithmById("quickdemforjp:quickdemforjp")
+    alg = registry.algorithmById("quickdemforjp:quickdemforjp")
     assert alg is not None
     assert alg.group() is None
     assert alg.groupId() is None
